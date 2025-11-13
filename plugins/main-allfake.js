ef7cbde7b04020ab91518c67cc1315d76894d0c9
dev-fedexz <devfedexyz@gmail.com>
2025-11-13 13:10:49 -0300

    Add files via upload

---
+++ plugins/main-allfake.js
@@ -0,0 +1,209 @@
+import pkg from '@whiskeysockets/baileys'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
+
+var handler = m => m
+handler.all = async function (m) {
+
+global.getBuffer = async function getBuffer(url, options) {
+try {
+options ? options : {}
+var res = await axios({
+method: "get",
+url,
+headers: {
+'DNT': 1,
+'User-Agent': 'GoogleBot',
+'Upgrade-Insecure-Request': 1
+},
+...options,
+responseType: 'arraybuffer'
+})
+return res.data
+} catch (e) {
+console.log(`Error : ${e}`)
+}}
+
+global.creador = 'wa.me/5491124918653'
+global.ofcbot = `Wa.me/${conn.user.jid.split('@')[0]}?text=.code`
+global.namechannel = 'Ｓʜᴀᴅᴏᴡ • ʙᴏᴛ ᴄʜᴀɴɴᴇʟ'
+global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => catalogo,)
+
+global.canalIdM = ["120363417186717632@newsletter"]
+global.canalNombreM = ["Ｓʜᴀᴅᴏᴡ • ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ᴏғғɪᴄɪᴀʟ"]
+global.channelRD = await getRandomChannel()
+
+global.d = new Date(new Date + 3600000)
+global.locale = 'es'
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
+global.mes = d.toLocaleDateString('es', {month: 'long'})
+global.año = d.toLocaleDateString('es', {year: 'numeric'})
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+
+global.emoji = '⧼❕⧽'
+global.emoji2 = '⧼❕⧽'
+global.emojis = [emoji, emoji2].getRandom()
+
+var canal = 'https://whatsapp.com/channel/0029VbBccU4545v2NVeKzN3U'  
+var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
+let correo = 'fedelanyt20@gmail.com'
+global.redes = [canal, comunidad, correo].getRandom()
+
+let category = "imagen"
+    
+var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 1: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 2: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 3: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 4: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 5: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 6: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 7: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 8: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 9: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 10: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 11: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 12: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 13: hour = '𝗕𝘂𝗲𝗻𝗼𝘀 𝗱𝗶𝗮𝘀 🌅'; break; case 14: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝘁𝗮𝗿𝗱𝗲𝘀 🌄'; break; case 15: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝘁𝗮𝗿𝗱𝗲𝘀 🌄'; break; case 16: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝘁𝗮𝗿𝗱𝗲𝘀 🌄'; break; case 17: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝘁𝗮𝗿𝗱𝗲𝘀 🌄'; break; case 18: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 19: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 20: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 21: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 22: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break; case 23: hour = '𝗕𝘂𝗲𝗻𝗮𝘀 𝗻𝗼𝗰𝗵𝗲𝘀 🌌'; break;}
+global.saludo = hour;
+
+global.nombre = m.pushName || 'Anónimo'
+global.taguser = '@' + m.sender.split("@s.whatsapp.net")
+var more = String.fromCharCode(8206)
+global.readMore = more.repeat(850)
+
+global.fkontak = {
+    key: {
+        participant: `0@s.whatsapp.net`,
+        ...(m.chat ? { remoteJid: `status@broadcast` } : {})
+    },
+    message: {
+        contactMessage: {
+            displayName: `Dev-fedexyz`,
+            vcard: `BEGIN:VCARD
+VERSION:3.0
+N:XL;Dev-fedexyz;;;
+FN:Dev-fedexyz
+item1.TEL;waid=5491124918653:5491124918653
+item1.X-ABLabel:Ponsel
+END:VCARD`,
+            jpegThumbnail: null,
+            thumbnail: null,
+            sendEphemeral: true
+        }
+    }
+}
+
+// === NAME STICKER INPORTANTE ===
+global.packsticker = `${nombre}`;
+global.packsticker2 = `🄲 Ｓｈａｄｏｗ - Ｂｏｔ`
+
+global.fake = {
+  contextInfo: { 
+    isForwarded: true, 
+    forwardedNewsletterMessageInfo: { 
+      newsletterJid: global.canalIdM, 
+      newsletterName: global.canalNombreM, 
+      serverMessageId: -1 
+    } 
+  }
+}
+
+// === GLOBAL IMG ===
+global.icono = [
+'https://raw.githubusercontent.com/El-brayan502/dat2/main/uploads/2ead00-1761433435253.jpg',
+'https://raw.githubusercontent.com/El-brayan502/dat2/main/uploads/a8b8ec-1761433485401.jpg',
+'https://raw.githubusercontent.com/El-brayan502/dat4/main/uploads/925053-1761433519795.jpg',
+].getRandom()
+
+global.menum = [ 
+'https://raw.githubusercontent.com/El-brayan502/dat2/main/uploads/8fe970-1761423379402.jpg',
+].getRandom()
+
+// === GLOBAL CHANNEL ===
+global.rcanal = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: 200,
+      newsletterName: channelRD.name,
+    },
+    externalAdReply: {
+      title: namev,
+      body: `${global.saludo}`,
+      thumbnailUrl: icono,
+      sourceUrl: '',
+      mediaType: 1,
+      renderLargerThumbnail: false
+    }
+  }
+}
+
+global.rcanalx = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: 201,
+      newsletterName: channelRD.name,
+    },
+    externalAdReply: {
+      title: namew,
+      body: `${global.saludo}`,
+      thumbnailUrl: icono,
+      sourceUrl: '',
+      mediaType: 1,
+      renderLargerThumbnail: false
+    }
+  }
+}
+
+// === GLOBAL CHANNEL ERRORR ===
+global.rcanalw = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: 202,
+      newsletterName: channelRD.name,
+    },
+    externalAdReply: {
+      title: erorr,
+      body: `${global.saludo}`,
+      mediaUrl: null,
+      description: null,
+      previewType: "PHOTO",
+      thumbnailUrl: menum,
+      mediaType: 1,
+      renderLargerThumbnail: false
+    },
+  },
+}
+
+global.rcanalv = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: 203,
+      newsletterName: channelRD.name,
+    },
+    externalAdReply: {
+      title: erorr1,
+      body: `${global.saludo}`,
+      mediaUrl: null,
+      description: null,
+      previewType: "PHOTO",
+      thumbnailUrl: menum,
+      mediaType: 1,
+      renderLargerThumbnail: false
+    },
+  },
+}
+
+}
+
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+}
+
+async function getRandomChannel() {
+let randomIndex = Math.floor(Math.random() * canalIdM.length)
+let id = canalIdM[randomIndex]
+let name = canalNombreM[randomIndex]
+return { id, name }
+}
