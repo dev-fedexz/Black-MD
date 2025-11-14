ef490186eca39cace32b3ff497c8aeab301e9fff
dev-fedexz <devfedexyz@gmail.com>
2025-11-14 20:59:14 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, args }) => {
             const status = execSync('git status --porcelain').toString().trim(); 
             if (status) { 
                 const conflictedFiles = status.split('\n').filter(line => 
-                    !line.includes('ItachiSession/') && 
+                    !line.includes('Shadow/Session/') && 
                     !line.includes('.cache/') && 
                     !line.includes('tmp/')
                 ); 
