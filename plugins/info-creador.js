637fa88113de1952b589bece7af0f540b488ca98
dev-fedexz <devfedexyz@gmail.com>
2025-11-14 21:41:40 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -4,10 +4,10 @@ VERSION:3.0
 FN:𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 ORG:𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 TITLE:Epictetus, Enchiridion — Chapter 1 (verse 1)
-EMAIL;type=INTERNET:brayanfree881@gmail.com
+EMAIL;type=INTERNET:fedelanyt20@gmail.com 
 TEL;type=CELL;waid=5491124918653:+5491124918653
 ADR;type=WORK:;;2-chōme-7-5 Fuchūchō;Izumi;Osaka;594-0071;Japan
-URL;type=WORK:https://www.instagram.com/naruyaizumi
+URL;type=WORK:https://www.instagram.com/dev-fedexyz13
 X-WA-BIZ-NAME:Ｓｈａｄｏｗ - Ｂｏｔ
 X-WA-BIZ-DESCRIPTION:🄲 𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘣𝘺 𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
@@ -36,8 +36,8 @@ END:VCARD`;
             },
             contextInfo: {
                 externalAdReply: {
-                    title: "© 2024–2025 Shadow Project",
-                    body: "Contacta al Propietario vía WhatsApp",
+                    title: "© 2024–2025 Shadow`S Project",
+                    body: "Contacta con el CEO del bot",
                     thumbnailUrl: "https://files.catbox.moe/1lkvoh.png",
                     mediaType: 1,
                     renderLargerThumbnail: true,
