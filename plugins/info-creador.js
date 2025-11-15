129dea3c47dff7e81777eefa8239458048457dd2
dev-fedexz <devfedexyz@gmail.com>
2025-11-14 21:28:45 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn }) => {
     const vcard = `BEGIN:VCARD
 VERSION:3.0
-FN:𝐇𝐚𝐬𝐡𝐢𝐫𝐚𝐦𝐚 𝐒𝐞𝐧𝐣𝐮
-ORG:𝐇𝐚𝐬𝐡𝐢𝐫𝐚𝐦𝐚 𝐒𝐞𝐧𝐣𝐮
+FN:𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
+ORG:𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 TITLE:Epictetus, Enchiridion — Chapter 1 (verse 1)
 EMAIL;type=INTERNET:brayanfree881@gmail.com
 TEL;type=CELL;waid=50231458537:+50231458537
 ADR;type=WORK:;;2-chōme-7-5 Fuchūchō;Izumi;Osaka;594-0071;Japan
 URL;type=WORK:https://www.instagram.com/naruyaizumi
-X-WA-BIZ-NAME:Brayan Uchiha
-X-WA-BIZ-DESCRIPTION:𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙗𝙮 𝘽𝙧𝙖𝙮𝙖𝙣 𝙪𝙘𝙝𝙞𝙝𝙖
+X-WA-BIZ-NAME:Ｓｈａｄｏｗ - Ｂｏｔ
+X-WA-BIZ-DESCRIPTION:🄲 𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘣𝘺 𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
 END:VCARD`;
 
@@ -36,9 +36,9 @@ END:VCARD`;
             },
             contextInfo: {
                 externalAdReply: {
-                    title: "© 2024–2025 Itachi Project",
+                    title: "© 2024–2025 Shadow Project",
                     body: "Contacta al Propietario vía WhatsApp",
-                    thumbnailUrl: "https://raw.githubusercontent.com/El-brayan502/dat1/main/uploads/c82ef3-1760861923954.jpg",
+                    thumbnailUrl: ".setbanner https://files.catbox.moe/1lkvoh.png",
                     mediaType: 1,
                     renderLargerThumbnail: true,
                 },
